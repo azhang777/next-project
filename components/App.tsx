dae19ac815e32dc4f5381726010d1d3fcb6e625c
Andy <andyzhanggm@gmail.com>
2023-12-27 21:50:04 -0500

    Set up next.js project to review react, linked bootstrap through layout in app router

---
+++ components/App.tsx
@@ -0,0 +1,11 @@
+import { Accordion } from "./Accordion";
+import { Banner } from "./Banner";
+
+export const App = () => {
+  return (
+    <main>
+      <Banner />
+      <Accordion />
+    </main>
+  );
+};
